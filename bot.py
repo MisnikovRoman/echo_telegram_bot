cc1f453d9c4e5f5ca4a721653e28b0bd9eb2dc6d
Roman Misnikov <rmisnikov@yotateam.com>
2020-07-12 15:53:00 +0300

    Fix error with chat id

--- bot.py
+++ bot.py
@@ -8,8 +8,8 @@ bot = telebot.TeleBot(BOT_TOKEN)
 # Add supported commands
 @bot.message_handler(commands = ['start','help'])
 def send_welcome(message):
-    bot.send_message(message.chat_id, "Здравствуйте, я бот-помощник в создании сборок")
-    message
+    bot.send_message(message.chat.id, "Здравствуйте, я бот-помощник в создании сборок")
+    
 
 # Run bot
 bot.polling()
