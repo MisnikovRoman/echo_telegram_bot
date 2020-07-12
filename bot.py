5e29ca972ae06a7d4e58b0cb559bda5a64fad5cd
Roman Misnikov <rmisnikov@yotateam.com>
2020-07-12 13:57:48 +0300

    Prepare for deploy

--- bot.py
+++ bot.py
@@ -1,10 +1,14 @@
 import telebot
+import os
 
-bot = telebot.TeleBot("TOKEN")
+# Create bot
+BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+bot = telebot.TeleBot(BOT_TOKEN)
 
+# Add supported commands
 @bot.message_handler(commands = ['start','help'])
 def send_welcome(message):
     bot.reply_to(message, message.text)
 
-
+# Run bot
 bot.polling()
