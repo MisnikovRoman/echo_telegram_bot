6b2a8ce3fb3cb125a518bf427cfa580754e1789c
Roman Misnikov <rmisnikov@yotateam.com>
2020-07-12 15:16:36 +0300

    Edit message

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ bot = telebot.TeleBot(BOT_TOKEN)
 # Add supported commands
 @bot.message_handler(commands = ['start','help'])
 def send_welcome(message):
-    bot.reply_to(message, message.text)
+    bot.reply_to(message, "Здравствуйте, я бот помощник в создании сборок")
 
 # Run bot
 bot.polling()
